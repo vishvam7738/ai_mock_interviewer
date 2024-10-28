a1dd4027ec9c45b871cb0968aaad13135a42122e
vishvam <vishvamaggarwal@gmail.com>
2024-10-28 19:45:34 +0530

    Authentication using Clerk

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h2>Hello World!</h2>
+      <Button>Submit</Button>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
   				foreground: 'hsl(var(--popover-foreground))'
   			},
   			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
+  				DEFAULT: '#4845D2',
   				foreground: 'hsl(var(--primary-foreground))'
   			},
   			secondary: {
