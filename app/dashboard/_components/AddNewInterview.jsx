8a4bc7b022577e649dfce5c9972fe185e39b97d3
vishvam <vishvamaggarwal@gmail.com>
2024-10-31 17:42:56 +0530

    Backend-setup

--- app/dashboard/_components/AddNewInterview.jsx
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
+import React from "react";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 const AddNewInterview = () => {
   return (
     <div>
-      <div className='p-10 border rounded-lg bg-secondary
-      hover:scale-105 hover:shadow-md cursor-pointer transition-all'>
-        <h2 className='text-lg text-center'>+ Add New</h2>
+      <div
+        className="p-10 border rounded-lg bg-secondary
+      hover:scale-105 hover:shadow-md cursor-pointer transition-all"
+      >
+        <h2 className="text-lg text-center">+ Add New</h2>
       </div>
+      <Dialog>
+        <DialogTrigger>Open</DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
-  )
-}
+  );
+};
 
-export default AddNewInterview
+export default AddNewInterview;
