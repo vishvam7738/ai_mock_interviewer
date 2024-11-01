0ecf0490f43d2a76178b5a9e45a02d2ba4c1d078
vishvam <vishvamaggarwal@gmail.com>
2024-11-01 15:15:56 +0530

    Added Form details

--- app/dashboard/_components/AddNewInterview.jsx
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 import {
   Dialog,
@@ -8,24 +9,57 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 
 const AddNewInterview = () => {
+  const [openDialog, setOpenDialog] = useState(false);
+  const [jobPosition,setJobPosition]=useState()
+  const [jobDesc,setJobDesc]=useState()
+  const [jobExperience,setJobExperience]=useState()
   return (
     <div>
       <div
         className="p-10 border rounded-lg bg-secondary
       hover:scale-105 hover:shadow-md cursor-pointer transition-all"
+        onClick={() => setOpenDialog(true)}
       >
         <h2 className="text-lg text-center">+ Add New</h2>
       </div>
-      <Dialog>
-        <DialogTrigger>Open</DialogTrigger>
-        <DialogContent>
+      <Dialog open={openDialog}>
+        <DialogContent className="max-w-2xl">
           <DialogHeader>
-            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogTitle className="text-2xl">
+              Tell us more about your job interviewing
+            </DialogTitle>
             <DialogDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
+              <form>
+                <div>
+                  <h2>
+                    Add Details about your job position/role, Job description
+                    and years of experience
+                  </h2>
+                  <div className="mt-7 my-3">
+                    <label>Job Role/Job Position</label>
+                    <Input placeholder="Ex. Full Stack Developer" required/>
+                  </div>
+                  <div className="my-3">
+                    <label>Job Description/ Teck Stack (In Short)</label>
+                    <Textarea placeholder="Ex. React, Angular, Node JS, MySQL etc. " />
+                  </div>
+                  <div className="my-3">
+                    <label>Years of Experience</label>
+                    <Input placeholder="Ex. 3" type="number" max="50" required />
+                  </div>
+                </div>
+                <div className="flex gap-5 justify-end">
+                  <Button type="button" variant="ghost" onClick={() => setOpenDialog(false)}>
+                    Cancel
+                  </Button>
+                  <Button type="submit">Start Interview</Button>
+                </div>
+              </form>
             </DialogDescription>
           </DialogHeader>
         </DialogContent>
