990d4a88a244f28c7907c8739d016b8a0c9bdb4b
vishvam <vishvamaggarwal@gmail.com>
2024-11-09 23:19:16 +0530

    Styled the landing page

--- app/dashboard/interview/[interviewID]/page.jsx
+++ app/dashboard/interview/[interviewID]/page.jsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 import { db } from "@/utils/db";
 import { MockInterviw } from "@/utils/schema";
 import { eq } from "drizzle-orm";
-import { WebcamIcon } from "lucide-react";
+import { Lightbulb, WebcamIcon } from "lucide-react";
 import React, { useEffect, useState } from "react";
 import Webcam from "react-webcam";
 
@@ -24,7 +24,7 @@ const Interview = ({ params }) => {
     setInterviewData(result[0]);
   };
   return (
-    <div className="my-10 flex justify-center flex-col items-center">
+    <div className="my-10 ">
       <h2 className="font-bold text-2xl">Let's Get Started</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
         <div className="flex flex-col my-5 gap-5 ">
@@ -42,8 +42,9 @@ const Interview = ({ params }) => {
             {interviewData.jobExperience}
           </h2>
           </div>
-          <div>
-            
+          <div className="p-5 border rounded-lg border-yellow-300 bg-yellow-100">
+            <h2 className="flex gap-2 items-center text-yellow-500"><Lightbulb/> <strong>Information</strong></h2>
+            <h2 className="mt-3 text-yellow-400">{process.env.NEXT_PUBLIC_INFORMATION}</h2>
           </div>
         </div>
         <div>
@@ -60,13 +61,18 @@ const Interview = ({ params }) => {
           ) : (
             <>
               <WebcamIcon className="h-72 w-full my-7 p-20 bg-secondary rounded-lg border" />
-              <Button onClick={() => setWebCamEnabled(true)}>
+              <Button variant="ghost" className="w-full" onClick={() => setWebCamEnabled(true)}>
                 Enable Web Cam and Microphone
               </Button>
             </>
           )}
         </div>
+        
       </div>
+      <div className="flex justify-end items-end">
+      <Button>Start Interview</Button>
+      </div>
+    
     </div>
   );
 };
