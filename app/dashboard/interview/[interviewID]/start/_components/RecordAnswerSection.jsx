9ebc43188996df5f530f8cc754ca547146e43437
vishvam <vishvamaggarwal@gmail.com>
2024-11-11 00:03:53 +0530

    Feedback page

--- app/dashboard/interview/[interviewID]/start/_components/RecordAnswerSection.jsx
+++ app/dashboard/interview/[interviewID]/start/_components/RecordAnswerSection.jsx
@@ -4,10 +4,22 @@ import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import Webcam from "react-webcam";
 import useSpeechToText from "react-hook-speech-to-text";
-import { Mic } from "lucide-react";
+import { Mic, StopCircle } from "lucide-react";
+import { toast } from "sonner";
+import { chatSession } from "@/utils/GeminiAIModel";
+import { db } from "@/utils/db";
+import { UserAnswer } from "@/utils/schema";
+import { useUser } from "@clerk/nextjs";
+import moment from "moment";
 
-const RecordAnswerSection = () => {
-    const [userAnswer,setUserAnswer]=useState('')
+const RecordAnswerSection = ({
+  mockInterviewQuestion,
+  activeQuestionIndex,
+  interviewData,
+}) => {
+  const [userAnswer, setUserAnswer] = useState("");
+  const { user } = useUser();
+  const [loading, setLoading] = useState(false);
   const {
     error,
     interimResult,
@@ -15,16 +27,73 @@ const RecordAnswerSection = () => {
     results,
     startSpeechToText,
     stopSpeechToText,
+    setResults,
   } = useSpeechToText({
     continuous: true,
     useLegacyResults: false,
   });
 
-  useEffect(()=>{
-    results.map((result)=>(
-        setUserAnswer(prevAns=>prevAns+result?.transcript)
-    ))
-  },[results])
+  useEffect(() => {
+    results.map((result) =>
+      setUserAnswer((prevAns) => prevAns + result?.transcript)
+    );
+  }, [results]);
+
+  useEffect(() => {
+    if (!isRecording && userAnswer.length > 10) {
+      UpdateUserAnswer();
+    }
+  }, [userAnswer]);
+
+  const StartStopRecording = async () => {
+    if (isRecording) {
+      stopSpeechToText();
+    } else {
+      startSpeechToText();
+    }
+  };
+
+  const UpdateUserAnswer = async () => {
+    console.log(userAnswer);
+    setLoading(true);
+    const feedbackPrompt =
+      "Question:" +
+      mockInterviewQuestion[activeQuestionIndex]?.Question +
+      ", User Answer:" +
+      userAnswer +
+      ",Depends on question and user answer for given interview question" +
+      " please give us rating for answer and feedback as area of improvement if any" +
+      "in just 3 to 5 lines to improve it in JSON format with rating field and feedback field";
+
+    const result = await chatSession.sendMessage(feedbackPrompt);
+    // const mockJsonResp=(result.response.text())
+    const mockJsonResp = result.response
+      .text()
+      .replace(/```json|```/g, "")
+      .trim();
+    console.log(mockJsonResp);
+    const JsonFeedbackResp = JSON.parse(mockJsonResp);
+
+    const resp = await db.insert(UserAnswer).values({
+      mockIdRef: interviewData?.mockId,
+      question: mockInterviewQuestion[activeQuestionIndex]?.Question,
+      correctAns: mockInterviewQuestion[activeQuestionIndex]?.answer,
+      userAns: userAnswer,
+      feedback: JsonFeedbackResp?.feedback,
+      rating: JsonFeedbackResp?.rating,
+      userEmail: user?.primaryEmailAddress?.emailAddress,
+      createdAt: moment().format("DD-MM-yyyy"),
+    });
+
+    if (resp) {
+      toast("User Answer recorded successfully");
+      setUserAnswer("");
+      setResults([]);
+    }
+    setResults([]);
+
+    setLoading(false);
+  };
 
   return (
     <div className="flex items-center justify-center flex-col">
@@ -44,15 +113,21 @@ const RecordAnswerSection = () => {
           }}
         />
       </div>
-      <Button variant="outline" className="my-10"
-      onClick={isRecording?stopSpeechToText:startSpeechToText}
+      <Button
+        disabled={loading}
+        variant="outline"
+        className="my-10"
+        onClick={StartStopRecording}
       >
         {isRecording ? (
           <h2 className="text-red-600 flex gap-2">
-            <Mic />Stop Recording
+            <StopCircle />
+            Stop Recording
           </h2>
         ) : (
-          "Record Answer"
+          <h2 className="text-primary flex gap-2 items-center">
+            <Mic /> Record Answer
+          </h2>
         )}
       </Button>
       {/* <h1>Recording: {isRecording.toString()}</h1>
@@ -66,7 +141,7 @@ const RecordAnswerSection = () => {
         {interimResult && <li>{interimResult}</li>}
       </ul> */}
 
-      <Button onClick={()=>console.log(userAnswer)}> Show User Answer</Button>
+      {/* <Button onClick={() => console.log(userAnswer)}> Show User Answer</Button> */}
     </div>
   );
 };
--- app/dashboard/interview/[interviewID]/start/page.jsx
+++ app/dashboard/interview/[interviewID]/start/page.jsx
@@ -1,44 +1,61 @@
 "use client";
-import { db } from '@/utils/db';
-import { MockInterviw } from '@/utils/schema';
-import { eq } from 'drizzle-orm';
-import React, { useEffect, useState, use } from 'react';
-import QuestionsSection from './_components/QuestionsSection';
-import RecordAnswerSection from './_components/RecordAnswerSection';
+import { db } from "@/utils/db";
+import { MockInterviw } from "@/utils/schema";
+import { eq } from "drizzle-orm";
+import React, { useEffect, useState, use } from "react";
+import QuestionsSection from "./_components/QuestionsSection";
+import RecordAnswerSection from "./_components/RecordAnswerSection";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const StartInterview = ({ params }) => {
-    const unwrappedParams = use(params); // Unwrap the params promise
-    const [interviewData, setInterviewData] = useState();
-    const [mockInterviewQuestion, setMockInterviewQuestion] = useState();
-    const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
+  const unwrappedParams = use(params); // Unwrap the params promise
+  const [interviewData, setInterviewData] = useState();
+  const [mockInterviewQuestion, setMockInterviewQuestion] = useState();
+  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
 
-    useEffect(() => {
-        GetInterviewDetails();
-    }, []);
+  useEffect(() => {
+    GetInterviewDetails();
+  }, []);
 
-    const GetInterviewDetails = async () => {
-        const result = await db
-          .select()
-          .from(MockInterviw)
-          .where(eq(MockInterviw.mockId, unwrappedParams.interviewID)); // Use unwrappedParams
+  const GetInterviewDetails = async () => {
+    const result = await db
+      .select()
+      .from(MockInterviw)
+      .where(eq(MockInterviw.mockId, unwrappedParams.interviewID)); // Use unwrappedParams
 
-        const jsonMockResp = JSON.parse(result[0].jsonMockResp);
-        console.log(jsonMockResp);
-        setMockInterviewQuestion(jsonMockResp);
-        setInterviewData(result[0]);
-    };
+    const jsonMockResp = JSON.parse(result[0].jsonMockResp);
+    console.log(jsonMockResp);
+    setMockInterviewQuestion(jsonMockResp);
+    setInterviewData(result[0]);
+  };
 
-    return (
-      <div>
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-10'>
-            <QuestionsSection
-              mockInterviewQuestion={mockInterviewQuestion}
-              activeQuestionIndex={activeQuestionIndex}
-            />
-            <RecordAnswerSection/>
-        </div>
+  return (
+    <div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+        <QuestionsSection
+          mockInterviewQuestion={mockInterviewQuestion}
+          activeQuestionIndex={activeQuestionIndex}
+        />
+        <RecordAnswerSection
+          mockInterviewQuestion={mockInterviewQuestion}
+          activeQuestionIndex={activeQuestionIndex}
+          interviewData={interviewData}
+        />
       </div>
-    );
+      <div className="flex justify-end gap-6 ">
+        {activeQuestionIndex > 0 && <Button onClick={()=>setActiveQuestionIndex(activeQuestionIndex-1)}>Previous Question</Button>}
+        {activeQuestionIndex != mockInterviewQuestion?.length - 1 && (
+          <Button onClick={()=>setActiveQuestionIndex(activeQuestionIndex+1)}>Next Question</Button>
+        )}
+        {activeQuestionIndex == mockInterviewQuestion?.length - 1 && (
+          <Link href={'/dashboard/interview/'+interviewData?.mockId+'/feedback'}>
+          <Button>End Interview</Button>
+          </Link>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default StartInterview;
