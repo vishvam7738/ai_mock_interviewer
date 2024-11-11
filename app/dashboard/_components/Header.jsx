a5600f99b67a3794ca763861971f81724c3c020b
vishvam <vishvamaggarwal@gmail.com>
2024-11-11 23:07:54 +0530

    Styled the HomePage

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -1,50 +1,48 @@
-"use client";
-import React, { useEffect } from "react";
-import Image from "next/image";
-import { UserButton } from "@clerk/nextjs";
-import { usePathname } from "next/navigation";
+"use client"
+import { UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React, { useEffect } from 'react'
 
-const Header = () => {
-  const path = usePathname();
-  useEffect(() => {
-    console.log(path);
-  }, []);
+function Header() {
+
+    const path=usePathname();
+    useEffect(()=>{
+        console.log(path)
+    },[])
+
   return (
-    <div className="flex p-4 items-center justify-between bg-secondary shadow-sm">
-      <Image src={"/logo.svg"} width={160} height={100} alt="logo" />
-      <ul className="hidden md:flex gap-6">
-        <li
-          className={`hover:text-primary hover:font-bold transition-all cursor-pointer
-            ${path == "/dashboard" && "text-primary font-bold"}
+    <div className='flex p-4 items-center justify-between bg-secondary shadow-sm'>
+        <Image src={'/logo1.png'} width={160} height={100} alt='logo' />
+        <ul className='hidden md:flex gap-6'>
+          <Link href={"/dashboard"}>
+            <li className={`hover:text-primary hover:font-bold transition-all
+            cursor-pointer
+            ${path=='/dashboard'&&'text-primary font-bold'}
             `}
-        >
-          Dashboard
-        </li>
-        <li
-          className={`hover:text-primary hover:font-bold transition-all cursor-pointer
-            ${path == "/dashboard/questions" && "text-primary font-bold"}
-            `}
-        >
-          Questions
-        </li>
-        <li
-          className={`hover:text-primary hover:font-bold transition-all cursor-pointer
-            ${path == "/dashboard/upgrade" && "text-primary font-bold"}
-            `}
-        >
-          Upgrade
-        </li>
-        <li
-          className={`hover:text-primary hover:font-bold transition-all cursor-pointer
-            ${path == "/dashboard/how" && "text-primary font-bold"}
-            `}
-        >
-          How it Works?
-        </li>
-      </ul>
-      <UserButton />
+            
+            >Dashboard</li>
+            </Link>
+            
+            <li className={`hover:text-primary hover:font-bold transition-all
+            cursor-pointer
+            ${path=='/dashboard/questions'&&'text-primary font-bold'}
+            `}>Questions</li>
+              <Link href={"/dashboard/upgrade"}>
+            <li className={`hover:text-primary hover:font-bold transition-all
+            cursor-pointer
+            ${path=='/dashboard/upgrade'&&'text-primary font-bold'}
+            `}>Upgrade</li>
+            </Link>
+            <li className={`hover:text-primary hover:font-bold transition-all
+            cursor-pointer
+            ${path=='/dashboard/how'&&'text-primary font-bold'}
+            `}>How it Works?</li>
+        </ul>
+        <UserButton/>
     </div>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
