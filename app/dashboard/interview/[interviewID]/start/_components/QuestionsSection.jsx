1907c74ff8c87e4470993ce76525523844b4fd05
vishvam <110680520+vishvam7738@users.noreply.github.com>
2025-02-01 21:39:51 +0530

    Bug fixes(question generation)

--- app/dashboard/interview/[interviewID]/start/_components/QuestionsSection.jsx
+++ app/dashboard/interview/[interviewID]/start/_components/QuestionsSection.jsx
@@ -35,10 +35,10 @@ const QuestionsSection = ({ mockInterviewQuestion, activeQuestionIndex }) => {
         </div>
 
         <h2 className="my-5 text-md md:text-lg">
-          {mockInterviewQuestion[activeQuestionIndex]?.question}
+          {mockInterviewQuestion[activeQuestionIndex]?.Question}
         </h2>
 
-        <Volume2 className="cursor-pointer" onClick={()=>textToSpeach(mockInterviewQuestion[activeQuestionIndex]?.question)}/>
+        <Volume2 className="cursor-pointer" onClick={()=>textToSpeach(mockInterviewQuestion[activeQuestionIndex]?.Question)}/>
 
         <div className="border rounded-lg p-5 bg-blue-100 mt-20">
           <h2 className="flex gap-2 items-center text-primary">
